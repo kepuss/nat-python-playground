4fa02b331f276a625f543a757252f9afaf24316e
Natalia <natalia.glina@myob.com>
2023-03-14 20:02:05 +1100

    ex38 solution

---
+++ ex38.py
@@ -0,0 +1,7 @@
+input_list = [[1, 2, -3], [-4, 0, 4], [0, 1, -5], [1, 1, 1], [-2, 4, -1]]
+
+for value in input_list:
+    if sum(value) == 0:
+        print('True')
+    else:
+        print('False')
