b9f763663c124ffa9bca7aa311cb4ccb75c4417d
Natalia <natalia.glina@myob.com>
2023-03-26 22:35:25 +1100

    added error handling to ex38 -> 39

--- ex39.py
+++ ex39.py
@@ -1,14 +1,19 @@
 input_list = [[1, 2, -3], [-4, 0, 4], [0, 1, -5], [1, 1, 1], [-2, 4, -1]]
 
-
 def check_sum_of_list_elements(input):
+    if len(input) != 3:
+        raise TypeError(
+            "One of the nested arrays has more or less than 3 integers!")
     return sum(input) == 0
 
 
 def check_sum_of_nested_list_elements(nested_list):
     result = []
+    if len(nested_list) == 0:
+        raise TypeError("There aren't any nested arrays! ;(")
     for element in nested_list:
         result.append(check_sum_of_list_elements(element))
     return result
+
 
 print(check_sum_of_nested_list_elements(input_list))
