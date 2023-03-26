4a8e4962159b9df332f287e4e322bb2f1f4b951e
Natalia <natalia.glina@myob.com>
2023-03-26 21:59:17 +1100

    changed logic of ex38

--- ex38.py
+++ ex38.py
@@ -1,10 +1,14 @@
 input_list = [[1, 2, -3], [-4, 0, 4], [0, 1, -5], [1, 1, 1], [-2, 4, -1]]
 
-def check_sum_of_nested_list_elements(input):
-    for value in input_list:
-        if sum(value) == 0:
-            print('True')
-        else:
-            print('False')
+
+def check_sum_of_list_elements(input):
+    return sum(input) == 0
+
+
+def check_sum_of_nested_list_elements(nested_list):
+    result = []
+    for element in nested_list:
+        result.append(check_sum_of_list_elements(element))
+    return result
 
 print(check_sum_of_nested_list_elements(input_list))
