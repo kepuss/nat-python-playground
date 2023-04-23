168565f575ab73b34317d1b996f9c470527396d0
Natalia <natalia.glina@myob.com>
2023-04-24 03:35:53 +1000

    fixed the function, added tests <3

--- ex8.py
+++ ex8.py
@@ -2,24 +2,23 @@ import re
 
 input_string = "The colors in my studyroom are blue, green, and yellow."
 words_pattern = '[a-z]+'
-words_pattern_int = '^\d{5}$'
-characters_pattern = '[,\s]+'
+characters_pattern = r'[,\s]+'
 
 
-def count_spaces_and_commas_in_string(string, words_pattern, characters_pattern):
+def split_string_into_words_and_separators(string, words_pattern, characters_pattern):
 
-    characters_pattern_validator = '[,\s]+'
+    characters_pattern_validator = r'[,\s]+'
     x = re.findall(words_pattern, string, flags=re.IGNORECASE)
     y = re.findall(characters_pattern, string)
     if any(char.isdigit() for char in words_pattern):
         raise TypeError("No integers are allowed!")
-    if not characters_pattern is characters_pattern_validator:
+    if not characters_pattern == characters_pattern_validator:
         raise ValueError("We should be looking for commas and spaces only!")
     return [x, y]
 
 
 try:
-    print(count_spaces_and_commas_in_string(
+    print(split_string_into_words_and_separators(
         input_string, words_pattern, characters_pattern))
 except Exception as e:
     print("An error has occurred.", e)
--- test_ex8.py
+++ test_ex8.py
@@ -1,10 +1,31 @@
-from ex8 import count_spaces_and_commas_in_string
+from ex8 import split_string_into_words_and_separators
 import pytest
 
+# def test_count_spaces_and_commas_in_string():
+#     input_string = "Hello, world!"
+#     words_pattern = '[a-z]+'
+#     characters_pattern = '[,\s]+'
+#     expected_output = (['Hello', 'world'], [', '])
+#     assert count_spaces_and_commas_in_string(input_string, words_pattern, characters_pattern) == expected_output
 
-def test_count_spaces_and_commas_in_string():
+def test_split_string_into_words_and_separators_matches_expected_output():
     input_string = "The colors in my studyroom are blue, green, and yellow."
     words_pattern = '[a-z]+'
-    characters_pattern = '[,\s]+'
-    assert count_spaces_and_commas_in_string(input_string, words_pattern, characters_pattern) == [['The', 'colors', 'in', 'my', 'studyroom', 'are', 'blue', 'green', 'and', 'yellow'], [' ', ' ', ' ', ' ', ' ', ' ', ', ', ', ', ' ']]
+    characters_pattern = r'[,\s]+'
+    expected_output = [['The', 'colors', 'in', 'my', 'studyroom', 'are', 'blue', 'green', 'and', 'yellow'], [' ', ' ', ' ', ' ', ' ', ' ', ', ', ', ', ' ']]
+    assert split_string_into_words_and_separators(input_string, words_pattern, characters_pattern) == expected_output
 
+
+def test_split_string_into_words_and_separators_raises_TypeError_for_integer_in_words_pattern():
+    with pytest.raises(TypeError):
+        input_string = "The colors in my studyroom are blue, green, and yellow."
+        words_pattern = '[1-9]|10'
+        characters_pattern = r'[,\s]+'
+        split_string_into_words_and_separators(input_string, words_pattern, characters_pattern)
+
+def test_split_string_into_words_and_separators_raises_ValueError_for_other_separators():
+    with pytest.raises(ValueError):
+        input_string = "The colors in my studyroom are blue, green, and yellow."
+        words_pattern = '[a-z]+'
+        characters_pattern = r'[;\s]+'
+        split_string_into_words_and_separators(input_string, words_pattern, characters_pattern)
